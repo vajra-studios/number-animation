a97e9a5df5f7accd4b96f1a5368ebc9dc0c1f79e
balajivenkatesh <mjbalajivenkatesh@gmail.com>
2022-12-08 22:13:50 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import ExamplesGrid from "./ExamplesGrid";
 import SpeedExample from "./SpeedExample";
 
 const pageStyles: CSS.Properties = {
+  width: "100vw",
+  height: "100vh",
+  background: "linear-gradient(to right, #ECE9E6, #FFFFFF)",
+};
+
+const pageContentStyles: CSS.Properties = {
   paddingLeft: "16px",
   paddingRight: "16px",
   margin: "0 auto",
   maxWidth: "600px",
+  paddingTop: "20px" // required for the h2 margin
 };
 
 const App = () => {
   return (
     <div style={pageStyles}>
-      <ExamplesGrid />
-      <hr />
-      <SpeedExample />
+      <div style={pageContentStyles}>
+        <ExamplesGrid />
+        <hr />
+        <SpeedExample />
+      </div>
     </div>
   );
 };
